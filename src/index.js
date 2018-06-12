612190b1b7081b1852008edd3fe169b223f8a68f
GennaMarco <marcodege97@gmail.com>
2018-06-12 18:16:43 +0200

    flavour form with selected default value+

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import NameForm from './NameForm';
 import EssayForm from './EssayForm';
+import FlavorForm from './FlavorForm';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<NameForm />, document.getElementById('root'));
 ReactDOM.render(<EssayForm />, document.getElementById('root2'));
+ReactDOM.render(<FlavorForm />, document.getElementById('root3'));
 registerServiceWorker();
