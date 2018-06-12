c7c22fe754f3875406941aa19ecaafdcca4bdc01
GennaMarco <marcodege97@gmail.com>
2018-06-12 16:18:14 +0200

    login handle button and mailbox logical operators

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import LoginControl from './LoginControl'
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function Mailbox(props) {
+    const unreadMessages = props.unreadMessages;
+    return (
+        <div>
+            <h1>Hello!</h1>
+            {unreadMessages.length > 0 &&
+            <h2>
+                You have {unreadMessages.length} unread messages.
+            </h2>
+            }
+        </div>
+    );
+}
+
+const messages = ['React', 'Re: React', 'Re:Re: React'];
+
+ReactDOM.render(<LoginControl />, document.getElementById('root'));
+ReactDOM.render(<Mailbox unreadMessages={messages}/>, document.getElementById('root2'));
 registerServiceWorker();
