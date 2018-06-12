12c728a6acf7eb4821d7533a39d11a2a965585ab
GennaMarco <marcodege97@gmail.com>
2018-06-12 18:11:43 +0200

    textarea form in root2

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import NameForm from './NameForm';
+import EssayForm from './EssayForm';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<NameForm />, document.getElementById('root'));
+ReactDOM.render(<EssayForm />, document.getElementById('root2'));
 registerServiceWorker();
