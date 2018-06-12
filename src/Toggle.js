46c6ac5f8396ae63b6a20be2c45881b755167837
GennaMarco <marcodege97@gmail.com>
2018-06-12 15:29:59 +0200

    handling events on button element

---
+++ src/Toggle.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+class Toggle extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {isToggleOn: true};
+
+        // This binding is necessary to make `this` work in the callback
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        this.setState(prevState => ({
+            isToggleOn: !prevState.isToggleOn
+        }));
+    }
+
+    deleteRow(id) {
+        console.log(id);
+        console.log(this);
+        this.setState(prevState => ({
+            isToggleOn: !prevState.isToggleOn
+        }));
+    }
+
+    render() {
+        return (
+            <div>
+                <button onClick={this.handleClick}>
+                    {this.state.isToggleOn ? 'ON' : 'OFF'}
+                </button>
+                <button onClick={this.deleteRow.bind(this, "id")}>
+                    Delete Row
+                </button>
+            </div>
+        );
+    }
+}
+
+export default Toggle;
