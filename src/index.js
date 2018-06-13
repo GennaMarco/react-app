23d50302fff9231dcc0ac24f2da6aa42a2164c10
GennaMarco <marcodege97@gmail.com>
2018-06-13 09:55:21 +0200

    multiple inputs in one form

--- src/index.js
+++ src/index.js
@@ -5,8 +5,10 @@ import NameForm from './NameForm';
 import EssayForm from './EssayForm';
 import FlavorForm from './FlavorForm';
 import registerServiceWorker from './registerServiceWorker';
+import Reservation from "./Reservation";
 
 ReactDOM.render(<NameForm />, document.getElementById('root'));
 ReactDOM.render(<EssayForm />, document.getElementById('root2'));
 ReactDOM.render(<FlavorForm />, document.getElementById('root3'));
+ReactDOM.render(<Reservation />, document.getElementById('root4'));
 registerServiceWorker();
