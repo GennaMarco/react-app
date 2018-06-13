1686263433a26e1757b766a3e879b1e951ff5490
GennaMarco <marcodege97@gmail.com>
2018-06-13 10:10:38 +0200

    locked input with delay

--- src/index.js
+++ src/index.js
@@ -4,11 +4,18 @@ import './index.css';
 import NameForm from './NameForm';
 import EssayForm from './EssayForm';
 import FlavorForm from './FlavorForm';
-import registerServiceWorker from './registerServiceWorker';
 import Reservation from "./Reservation";
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<NameForm />, document.getElementById('root'));
 ReactDOM.render(<EssayForm />, document.getElementById('root2'));
 ReactDOM.render(<FlavorForm />, document.getElementById('root3'));
 ReactDOM.render(<Reservation />, document.getElementById('root4'));
+ReactDOM.render(<input value="hi"/>, document.getElementById('root5'));
+
+// delay, after 5 second the input is editable
+setTimeout(function()
+{
+    ReactDOM.render(<input value={null} />, document.getElementById('root5'));
+}, 5000);
 registerServiceWorker();
