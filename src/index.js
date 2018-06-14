9c8551bd4edc624f44922fdcd16a73bce8a5b8bb
GennaMarco <marcodege97@gmail.com>
2018-06-14 16:20:16 +0200

    routing with libraries npm

--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Loadable from 'react-loadable';
+
+const Loading = () => <div>Loading...</div>;
+
+const Home = Loadable({
+    loader: () => import('./routes/Home'),
+    loading: Loading,
+});
+
+const About = Loadable({
+    loader: () => import('./routes/About'),
+    loading: Loading,
+});
+
+const App = () => (
+    <Router>
+        <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route path="/about" component={About}/>
+        </Switch>
+    </Router>
+);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
