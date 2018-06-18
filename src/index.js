296c77cc3abd6105a1a1f4462b33fe2409b071cf
GennaMarco <marcodege97@gmail.com>
2018-06-18 09:31:54 +0200

    multiple providers nested in multiple consumers

--- src/index.js
+++ src/index.js
@@ -1,8 +1,62 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+// Theme context, default to light theme
+const ThemeContext = React.createContext('light');
+
+// Signed-in user context
+const UserContext = React.createContext({
+    name: 'Guest',
+});
+
+class App extends React.Component {
+    render() {
+        const {signedInUser, theme} = this.props;
+
+        // App component that provides initial context values
+        return (
+            <ThemeContext.Provider value={theme}>
+                <UserContext.Provider value={signedInUser}>
+                    <Layout />
+                </UserContext.Provider>
+            </ThemeContext.Provider>
+        );
+    }
+}
+
+function Layout() {
+    return (
+        <div>
+            <aside>
+            </aside>
+            <Content />
+        </div>
+    );
+}
+
+// A component may consume multiple contexts
+function Content() {
+    return (
+        <ThemeContext.Consumer>
+            {theme => (
+                <UserContext.Consumer>
+                    {user => (
+                        <ProfilePage user={user} theme={theme} />
+                    )}
+                </UserContext.Consumer>
+            )}
+        </ThemeContext.Consumer>
+    );
+}
+
+function ProfilePage() {
+    return (
+        <div>
+
+        </div>
+    );
+}
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
