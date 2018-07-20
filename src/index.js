5b98f3cda58617d18d3a515719c9a6e217023043
GennaMarco <marcodege97@gmail.com>
2018-07-20 11:58:17 +0200

    list item with redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import configureStore from './store/configureStore';
+import ItemList from './components/ItemList';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const store = configureStore(); // You can also pass in an initialState here
+
+render(
+    <Provider store={store}>
+        <ItemList />
+    </Provider>,
+    document.getElementById('app')
+);
